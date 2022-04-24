b59c0066f77ba21be814b1d4ce30d4528b15131e
Ruchan Yalçın <ruchany13@gmail.com>
2022-04-24 14:42:28 +0300

    create search page

---
+++ Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/Views/SearchPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Teknosa_Clone.Views
+{	
+	public partial class SearchPage : ContentPage
+	{	
+		public SearchPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
