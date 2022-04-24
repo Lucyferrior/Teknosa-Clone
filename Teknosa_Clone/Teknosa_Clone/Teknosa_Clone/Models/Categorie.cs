93f52a1e70bea2d5c392d0bad8caf9c91c3f3032
Sefa <sefa404e@gmail.com>
2022-04-24 23:14:45 +0300

    appshell tamamı yapıldı ve anasayfanın bir kısmı yapıldı

---
+++ Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/Models/Categorie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Teknosa_Clone.Models
+{
+    public class Categorie
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public ImageSource ImageSource { get; set; }
+    }
+}
---
+++ Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/ViewModels/MainPageViewMogel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows.Input;
+using Teknosa_Clone.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using Teknosa_Clone.Services;
+using Teknosa_Clone.Models;
+
+namespace Teknosa_Clone.ViewModels
+{
+    public class MainPageViewModel : ContentPage
+    {
+        public Collection<Categorie> Items { get; set; }
+        public MainPageViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+            Items = getCategories();
+        }
+        private Collection<Categorie> getCategories()
+        {
+            Collection<Categorie> _categories = new Collection<Categorie>();
+            _categories = new Collection<Categorie>
+            {
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "television",
+                    Text = "Televizyon, Ses ve Görüntü Sistemleri",
+                    ImageSource = ImageSource.FromFile("televizyon.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "telefon",
+                    Text = "Telefon",
+                    ImageSource = ImageSource.FromFile("telefon.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "beyaz",
+                    Text = "Beyaz Eşya ve Ankastre",
+                    ImageSource = ImageSource.FromFile("camasir.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "fotograf",
+                    Text = "Fotoğraf Makinesi ve Kamera",
+                    ImageSource = ImageSource.FromFile("fotograf.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "tablet",
+                    Text = "Bilgisayar ve Tablet",
+                    ImageSource = ImageSource.FromFile("tablet.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "supurge",
+                    Text = "Elektrikli Ev Aletleri",
+                    ImageSource = ImageSource.FromFile("supurge.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "kisiselBakım",
+                    Text = "Kişisel Bakım",
+                    ImageSource = ImageSource.FromFile("fon.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "sporOutdoor",
+                    Text = "Spor Outdoor",
+                    ImageSource = ImageSource.FromFile("scooter.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Oyun",
+                    Text = "Konsol, Oyun ve Oyuncu Ekipmanları",
+                    ImageSource = ImageSource.FromFile("ps.png")
+                },
+                new Categorie
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "muzik",
+                    Text = "Müzik Enstrüman ve Ekipman",
+                    ImageSource = ImageSource.FromFile("gitar.png")
+                }
+            };
+            return _categories;
+        }
+        public ICommand LoginCommand { get; }
+        private async void OnLoginClicked(object obj)
+        {
+            await Shell.Current.Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
