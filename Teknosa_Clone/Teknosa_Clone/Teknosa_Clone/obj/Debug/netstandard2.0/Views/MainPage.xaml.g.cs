23d2dab6487b6c5f8585c772f824bae55c7be3bf
Sefa <sefa404e@gmail.com>
2022-04-29 02:39:05 +0300

    Revert "Merge pull request #6 from Lucyferrior/Sefa"

--- Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/obj/Debug/netstandard2.0/Views/MainPage.xaml.g.cs
+++ Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/obj/Debug/netstandard2.0/Views/MainPage.xaml.g.cs
@@ -17,12 +17,12 @@ namespace Teknosa_Clone.Views {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.CollectionView deneme;
+        private global::Xamarin.Forms.StackLayout butonlar;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            deneme = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CollectionView>(this, "deneme");
+            butonlar = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "butonlar");
         }
     }
 }
