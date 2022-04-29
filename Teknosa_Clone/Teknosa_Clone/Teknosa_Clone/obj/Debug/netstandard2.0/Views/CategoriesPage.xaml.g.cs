6232af10bfee52708c9d5ba1da31ec8b916df207
Sefa <sefa404e@gmail.com>
2022-04-29 04:38:50 +0300

    sayfalar düzenlendi

--- Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/obj/Debug/netstandard2.0/Views/CategoriesPage.xaml.g.cs
+++ Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/obj/Debug/netstandard2.0/Views/CategoriesPage.xaml.g.cs
@@ -13,7 +13,7 @@
 namespace Teknosa_Clone.Views {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Views/CategoriesPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Views\\CategoriesPage.xaml")]
     public partial class CategoriesPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
