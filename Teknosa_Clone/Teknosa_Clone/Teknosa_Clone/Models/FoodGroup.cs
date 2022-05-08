a18336f11db971f3e7731c256fbbedee8d39b7ad
Ruchan Yalçın <ruchany13@gmail.com>
2022-05-08 11:32:36 +0300

    add some categories

--- Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/Models/FoodGroup.cs
+++ Teknosa_Clone/Teknosa_Clone/Teknosa_Clone/Models/FoodGroup.cs
@@ -60,39 +60,115 @@ namespace Teknosa_Clone.Models
         static FoodGroup()
         {
             ObservableCollection<FoodGroup> Groups = new ObservableCollection<FoodGroup>{
-                new FoodGroup("Telefon", "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125078519/125078519_1_MC/61250704.jpg"){
-                    new Food { Name = "Cep Telefonu", Icon="https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125078519/125078519_1_MC/61250704.jpg" },
-                    new Food { Name = "Giyilebilir Teknoloji", Icon="https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/780152467/780152467_0_MC/43140919.jpg" },
-                    new Food { Name = "Kulaklık", Icon="https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110263107/110263107_0_MC/1234.jpg" },
-                    new Food { Name = "Masaüstü ve Telsiz Telefon", Icon="https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/780450001/780450001_0_MC/5cb9da7ee14e43cab8929213e128cd08.jpg" },
-                    new Food { Name = "Telefon Aksesuar", Icon="https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/145059446/145059446_1_MC/34372092.jpg" },
-
+                new FoodGroup("Cep Telefonu", "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/125078519/125078519_1_MC/61250704.jpg")
+                {
+                    new Food{ Name = "Android Telefon"},
+                    new Food{ Name = "İOS Telefon"},
+                    new Food{ Name = "Tuşlu Telefon"}
                 },
+
+                new FoodGroup("Giyilebilir Teknoloji", "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/780152467/780152467_0_MC/43140919.jpg")
+                {
+                    new Food{ Name = "Akıllı Saat"},
+                    new Food{ Name = "Akıllı Bileklik"},
+                    new Food{ Name = "Saat ve Bileklik Aksesuarı"},
+                    new Food{ Name = "Akıllı Çocuk Saati"},
+                    new Food{ Name = "Akıllı Kıyafet ve Ayakkabı"},
+                    new Food{ Name = "Kol Bandı"},
+                    new Food{ Name = "Akıllı Gözlük"},
+                    new Food{ Name = "Sanal ve Artırılmış Gerçeklik"}
+                },
+
+                new FoodGroup("Kulaklık", "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/110263107/110263107_0_MC/1234.jpg")
+                {
+                    new Food{ Name = "Bluettoh Kulaklık Aksesuarı"},
+                    new Food{ Name = "Kulak İçi Kulaklık"},
+                    new Food{ Name = "Bluettoh Kulaklık"},
+                    new Food{ Name = "Kulaklık Aksesuarı"},
+                    new Food{ Name = "Kulak Üstü Kulaklık"}
+                },
+
+                new FoodGroup("Masaüstü ve Telsiz Telefon", "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/780450001/780450001_0_MC/5cb9da7ee14e43cab8929213e128cd08.jpg")
+                { },
+
+                new FoodGroup("Telefon Aksesuar", "https://reimg-teknosa-cloud-prod.mncdn.com/mnresize/600/600/productimage/145059446/145059446_1_MC/34372092.jpg")
+                {
+                    new Food{ Name = "Taşınabilir Şarj Aleti"},
+                    new Food{ Name = "Ekran Koruyucu"},
+                    new Food{ Name = "Selfie Çubuğu"},
+                    new Food{ Name = "Telefon Tutucu"},
+                    new Food{ Name = "Bluetooth Araç Kiti"},
+                    new Food{ Name = "Telefon Yedek Parça"},
+                    new Food{ Name = "Telefon Kılıfı"},
+                    new Food{ Name = "Telefon Oyun Aksesuarları"},
+                    new Food{ Name = "Şarj Aleti"},
+                    new Food{ Name = "Şarj Kablosu"},
+                },
+                //new FoodGroup("", "")
+                //{
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //    new Food{ Name = ""},
+                //},
+
 
             };
             All = Groups;
 
-            //ObservableCollection<FoodGroup> Groups2 = new ObservableCollection<FoodGroup>{
-            //    new FoodGroup("Laptop, Notebook"){
-            //    },
+            ObservableCollection<FoodGroup> Groups2 = new ObservableCollection<FoodGroup>{
+                new FoodGroup("Laptop, Notebook",""){
+                },
 
-            //    new FoodGroup("Masaüstü Bilgisayar"){
-            //        new Food { Name = "Laptop, Notebook", },
-            //        new Food { Name = "Masaüstü Bilgisayar", },
-            //        new Food { Name = "pear", },
-            //    }};
-            //All2 = Groups2;
+                new FoodGroup("Masaüstü Bilgisayar", ""){
+                    new Food { Name = "Apple iMac" },
+                    new Food { Name = "Hazır Sistemler" },
+                    new Food { Name = "Mini Masaüstü"},
+                    new Food{ Name = "All In One"},
+                },
 
-            //ObservableCollection<FoodGroup> Groups3 = new ObservableCollection<FoodGroup>{
-            //    new FoodGroup("Televizyon"){
-            //    },
+                new FoodGroup("Tablet", "")
+                {
+                },
 
-            //    new FoodGroup("Görüntü Sistemleri"){
-            //        new Food { Name = "Laptop, Notebook", },
-            //        new Food { Name = "Masaüstü Bilgisayar", },
-            //        new Food { Name = "pear", },
-            //    }};
-            //All3 = Groups3;
+                new FoodGroup("Oyuncu Bilgisayarı", "")
+                {
+                    new Food{ Name = "Gaming Masaüstü"},
+                    new Food{ Name = "Gaming Laptop"},
+                },
+
+                new FoodGroup("Bilgisayar Bileşenleri", "")
+                {
+                    new Food{ Name = "Anakart"},
+                    new Food{ Name = "Ekran Kartı"},
+                    new Food{ Name = "RAM"},
+                    new Food{ Name = "Güç Kaynağı"},
+                    new Food{ Name = "İşlemci"},
+                    new Food{ Name = "Harddisk"},
+                    new Food{ Name = "SSD"},
+                    new Food{ Name = "Bilgisayar Kasası"},
+                },
+
+
+            };
+            All2 = Groups2;
+
+            ObservableCollection<FoodGroup> Groups3 = new ObservableCollection<FoodGroup>{
+                new FoodGroup("Televizyon", ""){
+                },
+
+                new FoodGroup("Görüntü Sistemleri", ""){
+                    new Food { Name = "Laptop, Notebook", },
+                    new Food { Name = "Masaüstü Bilgisayar", },
+                    new Food { Name = "pear", },
+                }};
+            All3 = Groups3;
         }
 
 
